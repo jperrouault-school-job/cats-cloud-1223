24c5f83b9024890221113e2b878ae429a5204c8b
Jeremy <jeremy.perrouault@ascadis.fr>
2023-12-06 12:29:59 +0100

    JOUR 1 - PRODUIT SERVICE

--- PROJET/produit-service/src/main/java/fr/formation/produitservice/repository/CommentaireRepository.java
+++ PROJET/produit-service/src/main/java/fr/formation/produitservice/repository/CommentaireRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.formation.produitservice.model.Commentaire;
 
 public interface CommentaireRepository extends JpaRepository<Commentaire, String> {
+    // @Query("select c from Commentaire c where c.produitId = ?1")
     public List<Commentaire> findAllByProduitId(String produitId);
 }

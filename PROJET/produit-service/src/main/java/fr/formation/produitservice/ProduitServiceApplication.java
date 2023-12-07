ca5d644e8ddbc0cd0760cf9429cf4edd80fbd533
Jeremy <jeremy.perrouault@ascadis.fr>
2023-12-07 14:15:49 +0100

    JOUR 2 - DEMO KAFKA CONSUMER

--- PROJET/produit-service/src/main/java/fr/formation/produitservice/ProduitServiceApplication.java
+++ PROJET/produit-service/src/main/java/fr/formation/produitservice/ProduitServiceApplication.java
@@ -1,13 +1,26 @@
 package fr.formation.produitservice;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
 @EnableFeignClients // Active les annotations @FeignClient
+@Log4j2
 public class ProduitServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProduitServiceApplication.class, args);
+    }
+
+    @Bean //("autreChose")
+    Consumer<String> onTopicDemo() {
+        return evt -> {
+            log.debug("Message reçu : {}", evt);
+        };
     }
 }
